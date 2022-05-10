02af8359a7ee62afb772176d54ed41108ad05acf
Nikhil <nikhilsnayak3473@gmail.com>
2022-05-10 09:21:20 +0530

    10-05-2022

---
+++ FRONTEND/practice/static/js/script.js
@@ -0,0 +1,100 @@
+// let timeToggler = document.getElementById('timeToggler');
+// let timeSpan = document.getElementById('time');
+// timeToggler.addEventListener('click', ()=>{
+//     if(timeToggler.innerHTML == 'Hide-Time'){
+//         timeSpan.style.display = 'none';
+//         timeToggler.innerText = 'Show-Time';
+//     }else{
+//         timeSpan.style.display = 'inline';
+//         timeToggler.innerText = 'Hide-Time';
+//     }
+    
+// });
+
+
+// function time() {
+//     time = new Date()setInterval(time,1000);
+//     document.getElementById('time').innerHTML = time;
+    
+// }
+
+// setInterval(time,1000);
+// const array1 = new Array("mnikhil", "hii", "sdgfv", "errgrg");
+
+
+// print(array1)
+// array1.push("UIFUIREHG")
+// console.log("after push method")
+// print(array1)
+
+// function print(array) {
+
+//     for (let index = 0; index < array.length; index++) {
+//         console.log(array[index])
+
+//     }
+
+// }
+
+
+// let arr = ['ddd','wdf','wdw']
+
+// arr.splice(1,0,'ccc')
+// console.log(arr)
+
+// object using literal
+// const car  ={
+//     name:"mtcar",
+//     price : 700000,
+//     acc : function(){
+//         console.log("car is acclerating");
+//     }
+
+  
+// };
+
+// console.log(car);
+
+// // object using new
+
+const obj = new Object();
+obj.name = "myobj";
+obj.id = 1;
+obj.call = function(){
+    console.log("i am groot")
+}
+
+console.log(obj)
+
+
+// // uing functions
+// function object(name,age,id){
+//     this.name = name;
+//     this.age = age;
+//     this.id = id;
+// }
+
+
+// let obj_ref = new object('nikhi',20,1);
+// console.log(obj_ref);
+
+
+// let a = function f1() {
+//     console.log("i am a fun")
+
+// }
+
+// console.log(a);
+// console.log(a());
+
+
+
+// DOM
+
+// document.write("hii")
+// document.getElementById("id")
+// document.getElementsByClassName("className")
+// document.getElementsByTagName("tagName")
+// document.querySelector("css selector name")
+// document.querySelectorAll("css selector name")
+// document.getElementsByName('name')
